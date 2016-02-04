2a0aae711a66535a4a859e463c003346128359b7
Katarina1234 <kat9234@gmail.com>
2016-02-04 13:12:46 -0800

    Update app.js
    Lines from tutorial, not relevant to us here

--- app.js
+++ app.js
@@ -2,13 +2,7 @@
  * Created by BryanC on 2016-01-31.
  */
 
-var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', [
-    '$scope',
-    function($scope){
-        $scope.test = 'Hello world!';
-    }]);
 
 var express = require('express');
 var path = require('path');

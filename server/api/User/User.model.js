e59bffe91a8308bb97a926680218356342e8ce1d
hounslow <hounslowmatthew@gmail.com>
2016-02-10 18:10:50 -0800

    UserSchema updated with new functions, and passport, passport-local, and jsonwebtoken added to package

--- server/api/User/User.model.js
+++ server/api/User/User.model.js
@@ -1,11 +1,42 @@
 'use strict';
 
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
+var crypto = require('crypto');
 
 var UserSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  username: {type: String, lowercase: true, unique: true},
+  //name: String,
+  //info: String,
+  //active: Boolean
+  hash: String,
+  salt: String
 });
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+  return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, 'SECRET');
+};
+
+var jwt = require('jsonwebtoken');
+
 
 export default mongoose.model('User', UserSchema);

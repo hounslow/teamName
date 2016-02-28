f63fb8c3c1ad2581e0c82206689dbaf7211f14a8
Katarina1234 <kat9234@gmail.com>
2016-02-28 03:33:38 -0800

    Profile UI in progress

--- client/app/profile/profile.controller.ts
+++ client/app/profile/profile.controller.ts
@@ -3,4 +3,28 @@
 angular.module('teamNameApp')
   .controller('ProfileCtrl', function ($scope) {
     $scope.message = 'Hello';
+    $scope.title = "Welcome to this demo!";
+    $scope.editorEnabled = false;
+
+    $scope.enableEditor = function() {
+      $scope.editorEnabled = true;
+      $scope.editableTitle = $scope.title;
+      $scope.editableMessage = $scope.message;
+      $scope.editableInterests = $scope.interests;
+    };
+
+    $scope.disableEditor = function() {
+      $scope.editorEnabled = false;
+    };
+
+    $scope.save = function() {
+      $scope.message = $scope.editableMessage;
+      $scope.title = $scope.editableTitle;
+      $scope.interests = $scope.editableInterests;
+      $scope.disableEditor();
+    };
+
+
   });
+
+// http://icelab.com.au/articles/click-to-edit-with-angularjs/

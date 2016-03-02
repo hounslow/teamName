dd0ff737ac9b4f4c70359ffdb5be9295e8711c7f
Katarina1234 <kat9234@gmail.com>
2016-03-02 01:04:24 -0800

    More login sstuff updated. Our register page now creates new users.

--- client/app/register/register.controller.ts
+++ client/app/register/register.controller.ts
@@ -1,6 +1,45 @@
 'use strict';
 
+class RegisterCtrl {
+  //start-non-standard
+  user = {};
+  errors = {};
+  submitted = false;
+  //end-non-standard
+
+  constructor(Auth, $location) {
+    this.Auth = Auth;
+    this.$location = $location;
+  }
+
+  register(form) {
+    this.submitted = true;
+
+    if (form.$valid) {
+      console.log('GOT HEREEEEEE'+this.user.name);
+
+      this.Auth.createUser({
+      name: this.user.name,
+        email: this.user.email,
+        password: this.user.password
+      })
+      .then(() => {
+        // Account created, redirect to home
+        this.$location.path('/profile');
+      })
+      .catch(err => {
+        err = err.data;
+        this.errors = {};
+
+        // Update validity of form fields that match the mongoose errors
+        angular.forEach(err.errors, (error, field) => {
+          form[field].$setValidity('mongoose', false);
+          this.errors[field] = error.message;
+        });
+      });
+    }
+  }
+}
+
 angular.module('teamNameApp')
-  .controller('RegisterCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('RegisterCtrl', RegisterCtrl);
--- client/app/register/register.ts
+++ client/app/register/register.ts
@@ -5,6 +5,14 @@ angular.module('teamNameApp')
     $routeProvider
       .when('/register', {
         templateUrl: 'app/register/register.html',
-        controller: 'RegisterCtrl'
+        controller: 'RegisterCtrl',
+        controllerAs: 'bm'
       });
-  });
+ // })
+ // .run(function($rootScope) {
+ //   $rootScope.$on('$routeChangeStart', function(event, next, current) {
+ //     if (next.name === 'logout' && current && current.originalPath && !current.authenticate) {
+ //       next.referrer = current.originalPath;
+ //     }
+ //   });
+});

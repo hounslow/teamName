6ecddcd9015297b81b730782a74a6c117e56f326
Katarina1234 <kat9234@gmail.com>
2016-03-22 03:55:11 -0700

    notifications for comic added to favourites

--- client/app/the-feed/the-feed.controller.ts
+++ client/app/the-feed/the-feed.controller.ts
@@ -17,8 +17,27 @@ class TheFeedCtrl {
 
   addToFavourites(Comic) {
     this.$http.post('/api/users/'+this.Auth.getCurrentUser()._id + '/my-favourites', {id: this.Auth.getCurrentUser()._id, myComics: Comic._id});
-  };
 
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification("Comic has been added to favourites!");
+    }
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification("Comic has been added to favourites!");
+        }
+      });
+    }
+  };
+//From https://developer.mozilla.org/en/docs/Web/API/notification
 
 }
 

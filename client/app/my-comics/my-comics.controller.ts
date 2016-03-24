8edd67994200699c78ed1d5607f3d6e10613a80c
Katarina1234 <kat9234@gmail.com>
2016-03-24 12:23:33 -0700

    my comics

--- client/app/my-comics/my-comics.controller.ts
+++ client/app/my-comics/my-comics.controller.ts
@@ -1,6 +1,68 @@
 'use strict';
 
+class MyComicsCtrl {
+  private comicsToShowDescription: string[];
+
+  constructor($scope, $http, Auth){
+    this.isContributor = Auth.isUser;
+    this.Auth = Auth;
+    this.$scope = $scope;
+    this.$http = $http;
+    this.comicsToShowDescription = [];
+    // Grab the initial set of available comics
+    $http.get('/api/users/me').then(response => {
+      this.me = response.data;
+      $scope.Comics = this.me.myComics;
+    });
+  }
+
+  deleteComic(Comic) {
+    this.$http.delete('/api/Comics/' + Comic._id);
+    window.location.href='/my-favourites';
+  };
+  addToShowDescription(comicId: string){
+    this.comicsToShowDescription.push(comicId);
+  };
+
+  showDescription(comicId: string):boolean{
+    console.log('this should be true '+ (-1 != this.comicsToShowDescription.indexOf(comicId)));
+    return (-1 != this.comicsToShowDescription.indexOf(comicId));
+  };
+
+  removeFromShowDescription(comicId: string){
+    console.log('zero element del'+ this.comicsToShowDescription[0]);
+    console.log('one element del'+ this.comicsToShowDescription[1]);
+    var indexOfDelete = this.comicsToShowDescription.indexOf(comicId);
+    this.comicsToShowDescription.splice(indexOfDelete, 1);
+    console.log('zero element after del'+ this.comicsToShowDescription[0]);
+    console.log('one element after del'+ this.comicsToShowDescription[1]);
+  };
+
+  addToFavourites(Comic) {
+    this.$http.post('/api/users/'+this.Auth.getCurrentUser()._id + '/my-favourites', {id: this.Auth.getCurrentUser()._id, myComics: Comic._id});
+
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification("Comic has been added to favourites!");
+    }
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification("Comic has been added to favourites!");
+        }
+      });
+    }
+  };
+//From https://developer.mozilla.org/en/docs/Web/API/notification
+
+}
+
 angular.module('teamNameApp')
-  .controller('MyComicsCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('MyComicsCtrl', MyComicsCtrl);
--- client/app/my-comics/my-comics.ts
+++ client/app/my-comics/my-comics.ts
@@ -6,6 +6,7 @@ angular.module('teamNameApp')
       .when('/my-comics', {
         templateUrl: 'app/my-comics/my-comics.html',
         controller: 'MyComicsCtrl',
+        controllerAs: 'mc',
         authenticate: 'user'
 	    });
   });

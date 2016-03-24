8518e790767f72ca8f486c352e04b94334253022
Katarina1234 <kat9234@gmail.com>
2016-03-24 10:56:54 -0700

    can load big images

--- client/app/create-a-comic/create-a-comic.controller.ts
+++ client/app/create-a-comic/create-a-comic.controller.ts
@@ -11,28 +11,28 @@ angular.module('teamNameApp')
       for (var i = 0; i<files.length; i++)
         (function(i){
         var reader = new FileReader();
-      reader.onloadend = function(e){
-        fileString[i] = e.target.result;
-        if (i == (files.length - 1)){
-          $http.post('/api/Comics', {name: name , content: fileString, description: description, contributors: Auth.getCurrentUser()._id } );
-          $window.location.href='/the-feed';}
-      };
-      reader.readAsDataURL(files[i]);
-    })(i);}
-  });
+  //    reader.onloadend = function(e){
+  //      fileString[i] = e.target.result;
+  //      if (i == (files.length - 1)){
+  //        $http.post('/api/Comics', {name: name , content: fileString, description: description, contributors: Auth.getCurrentUser()._id } );
+  //        $window.location.href='/the-feed';}
+  //    };
+  //    reader.readAsDataURL(files[i]);
+  //  })(i);}
+  //});
 
-//    reader.onloadend = function(e){
-//      fileString[i] = e.target.result;
-//      if (i == (files.length - 1)){
-//        $http.post('/api/Comics', {name: name , content: fileString, description: description, contributors: Auth.getCurrentUser()._id } )
-//          .success(function(comic) {
-//            $http.post('api/users/' + Auth.getCurrentUser()._id + '/my-comics', {id: Auth.getCurrentUser()._id, myComics: comic._id});
-//            $window.location.href='/the-feed';
-//      })}
-//    };
-//    reader.readAsDataURL(files[i]);
-//  })(i);}
-//});
+    reader.onloadend = function(e){
+      fileString[i] = e.target.result;
+      if (i == (files.length - 1)){
+        $http.post('/api/Comics', {name: name , content: fileString, description: description, contributors: Auth.getCurrentUser()._id } )
+          .success(function(comic) {
+            $http.post('api/users/' + Auth.getCurrentUser()._id + '/my-comics', {id: Auth.getCurrentUser()._id, myComics: comic._id});
+            $window.location.href='/the-feed';
+      })}
+    };
+    reader.readAsDataURL(files[i]);
+  })(i);}
+});
 
 
 

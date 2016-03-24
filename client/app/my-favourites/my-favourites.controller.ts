f87bff3eed2e5479a8f1e35f49d23446d7faa3aa
Katarina1234 <kat9234@gmail.com>
2016-03-24 12:59:47 -0700

    my comics is now functional, personal website and interests in profile wrap around, favourites has show description functionality

--- client/app/my-favourites/my-favourites.controller.ts
+++ client/app/my-favourites/my-favourites.controller.ts
@@ -1,11 +1,14 @@
 'use strict';
 
 class MyFavouritesCtrl{
+   private comicsToShowDescription: string[];
+
 
   constructor(Auth, $location, $http, $scope) {
     this.Auth = Auth;
     this.$location = $location;
     this.$http = $http;
+    this.comicsToShowDescription = [];
     $http.get('/api/users/me').then(response => {
       this.me = response.data;
 //      if (this.me.myFavorites != null) {
@@ -16,6 +19,24 @@ class MyFavouritesCtrl{
       $scope.myFavourites = this.me.myFavourites;
     });
   }
+
+  addToShowDescription(comicId: string){
+    this.comicsToShowDescription.push(comicId);
+  };
+
+  showDescription(comicId: string):boolean{
+    console.log('this should be true '+ (-1 != this.comicsToShowDescription.indexOf(comicId)));
+    return (-1 != this.comicsToShowDescription.indexOf(comicId));
+  };
+
+  removeFromShowDescription(comicId: string){
+    console.log('zero element del'+ this.comicsToShowDescription[0]);
+    console.log('one element del'+ this.comicsToShowDescription[1]);
+    var indexOfDelete = this.comicsToShowDescription.indexOf(comicId);
+    this.comicsToShowDescription.splice(indexOfDelete, 1);
+    console.log('zero element after del'+ this.comicsToShowDescription[0]);
+    console.log('one element after del'+ this.comicsToShowDescription[1]);
+  };
 
   removeFromFavourites(comic: string){
     console.log('comic id in my fav controller beforeeeeee '+comic._id);

0cf50edb34a539514f47cbc7e3d837cb94310ca6
bryan-kc <sendtobryanchiu@gmail.com>
2016-03-29 19:29:20 -0700

    added events and controller in .ts

---
+++ server/api/Comic/Comic.events.ts
@@ -0,0 +1,41 @@
+/**
+ * Comic model events
+ */
+
+'use strict';
+
+import {EventEmitter} from 'events';
+var Comic = require('./Comic.model');
+
+class ComicEvents {
+
+	constructor(){
+
+		var ComicEvents = new EventEmitter();
+
+		// Set max event listeners (0 == unlimited)
+		ComicEvents.setMaxListeners(0);
+
+		// Model events
+		var events = {
+		  'save': 'save',
+		  'remove': 'remove'
+		};
+
+		// Register the event emitter to the model events
+		for (var e in events) {
+		  var event = events[e];
+		  Comic.schema.post(e, emitEvent(event));
+		};
+		
+	}
+
+	emitEvent(event) {
+	  return function(doc) {
+	    ComicEvents.emit(event + ':' + doc._id, doc);
+	    ComicEvents.emit(event, doc);
+	  }
+	};
+}
+
+export default ComicEvents;

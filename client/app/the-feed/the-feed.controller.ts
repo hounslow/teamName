4d9893402e9e6ec2b2dc9c495f9f438cd7dcebcb
Katarina1234 <kat9234@gmail.com>
2016-03-29 19:33:11 -0700

    changed favourites message

--- client/app/the-feed/the-feed.controller.ts
+++ client/app/the-feed/the-feed.controller.ts
@@ -53,14 +53,14 @@ class TheFeedCtrl {
     // Let's check whether notification permissions have already been granted
     else if (Notification.permission === "granted") {
       // If it's okay let's create a notification
-      var notification = new Notification("Comic has been added to favourites!");
+      var notification = new Notification("Comic has been added to your favourites!");
     }
     // Otherwise, we need to ask the user for permission
     else if (Notification.permission !== 'denied') {
       Notification.requestPermission(function (permission) {
         // If the user accepts, let's create a notification
         if (permission === "granted") {
-          var notification = new Notification("Comic has been added to favourites!");
+          var notification = new Notification("Comic has been added to your favourites!");
         }
       });
     }

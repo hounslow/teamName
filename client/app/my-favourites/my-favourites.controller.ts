efe51d2511e4d49c39626186ac860ae54b170fc8
NicDDDD <nic.dembek@hotmail.com>
2016-03-31 00:09:47 -0700

    editing almost done

--- client/app/my-favourites/my-favourites.controller.ts
+++ client/app/my-favourites/my-favourites.controller.ts
@@ -40,11 +40,11 @@ class MyFavouritesCtrl{
   };
 
   editComic(Comic){
-    this.comic.setComic(Comic._id);
-    console.log(this.Auth.getCurrentUser()._id);
+    //console.log(this.Auth.getCurrentUser()._id);
     this.$http.post('/api/Comics/' + Comic._id + '/contributors' ,{contributors: this.Auth.getCurrentUser()._id, notSaved: false});
     this.$http.post('api/users/' + this.Auth.getCurrentUser()._id + '/my-comics', {id: this.Auth.getCurrentUser()._id, myComics: Comic._id});
-    //window.location.href='/create-a-comic';
+    this.comic.setComic(Comic._id);
+    this.$location.path('/edit');
   }
 
   showDescription(comicId: string):boolean{
